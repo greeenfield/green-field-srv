27722cd9259005810621df77208fc9630df280a5
hyozzang2 <janghyooao@gmail.com>
2022-04-09 10:14:19 +0900

    userRepository save 메소드 수정

--- src/modules/user/infrastructure/repositories/user.repository.ts
+++ src/modules/user/infrastructure/repositories/user.repository.ts
@@ -1,10 +1,12 @@
 import { getRepository, getConnection } from 'typeorm'
+import { plainToInstance } from 'class-transformer'
 import { Inject } from '@nestjs/common'
 
 import { UserRepository } from '#modules/user/domain/repository'
 import { UserFactory } from '#modules/user/domain/factory'
 import { User } from '#modules/user/domain/user'
 import { UserEntity } from '#modules/user/infrastructure/entities/user.entity'
+import { UserProfileEntity } from '#modules/user/infrastructure/entities/profile.entity'
 
 export class UserRepositoryImplement implements UserRepository {
   constructor(@Inject(UserFactory) private readonly userFactory: UserFactory) {}
@@ -15,7 +17,12 @@ export class UserRepositoryImplement implements UserRepository {
   }
 
   async save(data: User): Promise<void> {
-    await getRepository(UserEntity).save(this.modelToEntity(data))
+    const profileEntity = plainToInstance(UserProfileEntity, data.properties().profile)
+
+    await Promise.all([
+      getRepository(UserEntity).save(this.modelToEntity(data)),
+      getRepository(UserProfileEntity).save(profileEntity),
+    ])
   }
 
   async updatePassword(id: string, password: string): Promise<void> {
@@ -41,7 +48,7 @@ export class UserRepositoryImplement implements UserRepository {
   }
 
   private modelToEntity(model: User): UserEntity {
-    return model.properties()
+    return plainToInstance(UserEntity, model.properties())
   }
 
   private entityToModel(entity: UserEntity): User {
